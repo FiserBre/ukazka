f74ccff400ce38760aeeab1ccd188129eb69fced
FiserBre <fiserbretislav@email.cz>
2025-12-03 11:55:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,15 +31,7 @@ function updateTheme(){
 
   themeIcon.innerHTML = isDark
     ? `<path d="M22 15.8442C20.6866 16.4382 19.2286 16.7688 17.6935 16.7688C11.9153 16.7688 7.23116 12.0847 7.23116 6.30654C7.23116 4.77135 7.5618 3.3134 8.15577 2C4.52576 3.64163 2 7.2947 2 11.5377C2 17.3159 6.68414 22 12.4623 22C16.7053 22 20.3584 19.4742 22 15.8442Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>`
-    : `<circle cx="12" cy="12" r="6"></circle>
-       <line x1="12" y1="1" x2="12" y2="3"/>
-       <line x1="12" y1="21" x2="12" y2="23"/>
-       <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/>
-       <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/>
-       <line x1="1" y1="12" x2="3" y2="12"/>
-       <line x1="21" y1="12" x2="23" y2="12"/>
-       <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/>
-       <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/>`;
+    : `<path d="M12 2V4M12 20V22M4 12H2M6.31412 6.31412L4.8999 4.8999M17.6859 6.31412L19.1001 4.8999M6.31412 17.69L4.8999 19.1042M17.6859 17.69L19.1001 19.1042M22 12H20M17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>`;
 
   hamburger.querySelectorAll('span').forEach(span=>span.style.background=isDark?'#fff':'#111');
 }
